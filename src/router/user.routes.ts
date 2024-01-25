5af0b6b43994c73b9377640d74f141af57eb76af
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-24 22:34:22 -0300

    feat: including notes on how to separate services

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -3,10 +3,16 @@ import { UserService } from '../service/user.service';
 
 const router = Router();
 
-const service = new UserService(router);
+const service = new UserService();
 
 router.post('/', async (request, response) => {
   const { name, email, password, photo } = request.body;
+
+  // 1 - Buscar se existe um usuario com email
+  // 2 - Se existir retornar erro
+  // 3 - Criar um hash para senha
+  // 4 - Salvar no banco de dados
+  // O que for verbo eu crio um serviço
 
   const newUser = await service.created(name, email, password, photo);
 
