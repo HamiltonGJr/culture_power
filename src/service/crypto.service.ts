b27c906f930dcdbf87a119b87220a373bc689e60
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-25 19:08:51 -0300

    refactor: adjusting the password hash code

--- src/service/crypto.service.ts
+++ src/service/crypto.service.ts
@@ -1,13 +1,8 @@
 import { hash } from 'bcrypt';
 
 export class Crypto {
-  async cryptoPassword (password: string): Promise<any> {
-    try {
+  async cryptoPassword (password: string) {
       const passwordHashed = await hash(password, 8);
-
       return passwordHashed;
-    }catch (error) {
-      console.log(error);
-    };
   };
 };
