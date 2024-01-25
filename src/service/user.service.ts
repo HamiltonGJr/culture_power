82f7804ba43c4a799f7fbf66dc40f76ec8b9dd92
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-25 19:10:43 -0300

    refactor: refactoring the code, adjusting the functions of each code in the service
    Obrigado Vinicius por ter me ajudado nesta parte

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,41 +1,20 @@
 import { User } from '../model/user';
-import { Crypto } from './crypto.service';
-
-const crypto = new Crypto();
+import { UserRepository } from '../repository/user.repository';
 
 export class UserService {
-  async searchRegisteredEmail(email: string): Promise<any> {
-    try {
-      const thisUserExists = await User.findOne({ email });
-
-      return thisUserExists;
-    } catch (error) {
-      console.log(error);
-    };
+  // Fazer uma interface
+  repository: UserRepository
+  constructor(repository: UserRepository) {
+    this.repository = repository
   };
 
-  async created(name: string, email: string, password: string, photo: string): Promise<any> {
-    try {
-      await this.searchRegisteredEmail(email);
+  async findUserByEmail(email: string) {
+      const existUser = await User.findOne({ email });
+      return existUser;
+  };
 
-      if (email != null) {
-        const messege = 'Email already registered, try again!';
-
-        return messege;
-      };
-
-      await crypto.cryptoPassword(password);
-
-      const newUser = await new User({
-        name,
-        email,
-        password,
-        photo
-      }).save();
-
-      return newUser;
-    } catch (error) {
-      console.log(error);
-    };
+  async create(name: string, email: string, password: string, photo: string) {
+      const user = await this.repository.save(name, email, password, photo);
+      return user;
   };
 };
