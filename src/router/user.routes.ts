17b15c68364e6350f6468585416183ea62167b3f
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-25 22:24:48 -0300

    feat: including the yup validation scheme, directly in the route
    Será refatorado futuramente, tanto que tem uma pasta só para isso

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { UserService } from '../service/user.service';
 import { UserRepository } from '../repository/user.repository';
 import { Crypto } from '../service/crypto.service';
+import * as Yup from 'yup';
+import { IUserDTO } from '../entitie/user.interf';
 
 const router = Router();
 
@@ -10,19 +12,34 @@ const service = new UserService(repository);
 const crypto = new Crypto();
 
 router.post('/', async (request, response) => {
-  const { name, email, password, photo } = request.body;
+  const userQuerySchema = Yup.object<IUserDTO>({
+    name: Yup.string().required(),
+    email: Yup.string().required().email(),
+    password: Yup.string().required(),
+    jewelsAmount: Yup.number().default(0),
+    products: Yup.object(),
+    photo: Yup.string().required()
+  });
 
-  const existUser = await service.findUserByEmail(email);
-  if (existUser != null) {
-    response.status(409).send({ messege: 'Conflict: User with the provided email already exists. Please choose a different email.' });
-    return;
+  try {
+    await userQuerySchema.validate(request.body);
+    const { name, email, password, photo } = request.body;
+
+    const existUser = await service.findUserByEmail(email);
+    if (existUser != null) {
+      response.status(409).send({ messege: 'Conflict: User with the provided email already exists. Please choose a different email.' });
+      return;
+    };
+
+    const passwordHashed = await crypto.cryptoPassword(password);
+
+    const newUser = await service.create(name, email, passwordHashed, photo);
+
+    response.status(201).send({ user: newUser });
+  } catch (error) {
+    const { errors }: any = error
+    response.status(400).send({error: errors});
   };
-
-  const passwordHashed = await crypto.cryptoPassword(password);
-
-  const newUser = await service.create(name, email, passwordHashed, photo);
-
-  response.status(201).send({ user: newUser });
 });
 
 export default router;
