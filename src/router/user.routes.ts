30a7c13a21f41ea416978408acf52a292cfb0b78
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-29 18:11:06 -0300

    refactor: code refactoring in the routes part, making it cleaner and more functional

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -2,27 +2,16 @@ import { Router } from 'express';
 import { UserService } from '../service/user.service';
 import { UserRepository } from '../repository/user.repository';
 import { Crypto } from '../service/crypto.service';
-import * as Yup from 'yup';
-import { IUserDTO } from '../entitie/user.interf';
+import { UserSchema } from '../schema/user.schema';
 
 const router = Router();
 
 const repository = new UserRepository();
 const service = new UserService(repository);
-const crypto = new Crypto();
 
 router.post('/', async (request, response) => {
-  const userQuerySchema = Yup.object<IUserDTO>({
-    name: Yup.string().required(),
-    email: Yup.string().required().email(),
-    password: Yup.string().required(),
-    jewelsAmount: Yup.number().default(0),
-    products: Yup.object(),
-    photo: Yup.string().required()
-  });
-
   try {
-    await userQuerySchema.validate(request.body);
+    await UserSchema.validate(request.body);
     const { name, email, password, photo } = request.body;
 
     const existUser = await service.findUserByEmail(email);
@@ -31,7 +20,7 @@ router.post('/', async (request, response) => {
       return;
     };
 
-    const passwordHashed = await crypto.cryptoPassword(password);
+    const passwordHashed = await new Crypto().cryptoPassword(password);
 
     const newUser = await service.create(name, email, passwordHashed, photo);
 
