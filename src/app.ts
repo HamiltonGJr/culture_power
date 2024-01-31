fce6859bc585ab9a1592cd85a6874dac14607847
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-30 21:23:15 -0300

    feat: implementing the logger in the route index

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './router/index.routes';
+import { logger } from './middleware/logger';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(logger);
 app.use(routes);
 
 export default app;
