5d85e20b966c6509356d8bc9ca1a65af28c0d150
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-31 23:19:46 -0300

    feat: including the minimum number of characters the password needs to have

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -5,7 +5,7 @@ import { TypedRequest } from '../util/typedRequest';
 const UserSchema = Yup.object<IUserDTO>({
   name: Yup.string().required(),
   email: Yup.string().required().email(),
-  password: Yup.string().required(),
+  password: Yup.string().required().min(8),
   jewelsAmount: Yup.number().default(0),
   products: Yup.object(),
   photo: Yup.string().required()
