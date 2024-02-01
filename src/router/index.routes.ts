b0cde238ee00723fd3cde10dc25cd31d297ffecd
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-01-31 23:18:29 -0300

    feat: entering the authentication route

--- src/router/index.routes.ts
+++ src/router/index.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from './user.routes';
+import authRouter from './auth.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
+routes.use('/auth', authRouter);
 
 export default routes;
