fd420318d2ef74712be232799ad64cd95efc94d0
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-02 20:10:40 -0300

    feat: token implementation on the route

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -1,19 +1,22 @@
 import { Router } from 'express';
 import { UserService } from '../service/user.service';
-import { Crypto } from '../service/crypto.service';
+import { Crypto } from '../provider/crypto';
 import { UserRepository } from '../repository/user.repository';
 import * as userSchema from '../schema/user.schema';
 import validateRouter from '../middleware/validateRouter';
+import { auth } from '../middleware/auth';
 
 const router = Router();
 
 const repository = new UserRepository();
 const service = new UserService(repository);
 
-router.post('/', validateRouter(userSchema.CreatePerson.schema), async (request, response) => {
+router.post('/', validateRouter(userSchema.CreatePerson.schema), auth, async (request, response) => {
+  console.log(`ID do usuário logado: ${request.body.userId}`);
+
   const { name, email, password, jewelsAmount, photo } = request.body;
 
-  const existUser = await repository.findUserByEmail(email);
+  const existUser = await service.userByEmail(email);
   if (existUser)
     return response.status(409).send({ message: 'Conflict: User with the provided email already exists. Please choose a different email.' });
 
