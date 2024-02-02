f04eaba78c5def17e753245cfee23d73f71609a4
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-02 20:09:43 -0300

    refactor: code adjustment and interface implementation

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,11 +1,12 @@
-import { User } from '../model/user';
+import { User } from '../model/User';
+import { IUserRepository } from './IUser.repository';
 
-export class UserRepository {
+export class UserRepository implements IUserRepository{
   async findUserByEmail(email: string) {
     return await User.findOne({ email }).exec();
   };
 
   async save(name: string, email: string, password: string, jewelsAmount: number, photo: string) {
-    return await new User({ name, email, password, jewelsAmount, photo }).save();
+    return new User({ name, email, password, jewelsAmount, photo }).save();
   };
 };
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,10 +1,14 @@
 import { UserRepository } from '../repository/user.repository';
+import { IUserService } from './IUser.service';
 
-export class UserService {
-  // Fazer uma interface
+export class UserService implements IUserService {
   repository: UserRepository;
   constructor(repository: UserRepository) {
     this.repository = repository;
+  };
+
+  async userByEmail (email: string) {
+    return await this.repository.findUserByEmail(email);
   };
 
   async create(name: string, email: string, password: string, jewelsAmount: number, photo: string) {
