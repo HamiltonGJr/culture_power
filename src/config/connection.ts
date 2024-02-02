4e60bf1437ea8bcf2421a7af3158d49f14143c5a
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-02 20:05:41 -0300

    refactor: remove eslint method

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 mongoose.connect(process.env.URI as string).then((value) => {
   console.log(`DB connected: Version ${value.version}`);
 });
