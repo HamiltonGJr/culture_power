cf603442b2b845b30f3f28d0b33f52bc644dcbda
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-06 22:19:00 -0300

    refactor: tweaking the code

--- src/router/auth.routes.ts
+++ src/router/auth.routes.ts
@@ -11,22 +11,27 @@ const router = Router();
 const repository = new UserRepository();
 const service = new UserService(repository);
 
-router.post('/', validateRouter(authSchema.CreatePerson.schema), async (request, response) => {
-  const { email, password } = request.body;
+router.post(
+  '/',
+  validateRouter
+  (authSchema.CreatePerson.schema),
+  async (request, response) => {
+    const { email, password } = request.body;
 
-  const user = await service.userByEmail(email);
-  if (!user)
-    return response.status(401).send({ message: 'Unauthorized: Invalid credentials. Check your email and password and try again.' });
+    const user = await service.userByEmail(email);
+    if (!user)
+      return response.status(401).send({ message: 'Unauthorized: Invalid credentials. Check your email and password and try again.' });
 
-  const thesePasswordsAreTheSame = await compare(password, user.password);
-  if(!thesePasswordsAreTheSame)
-    return response.status(401).send({ message: 'Unauthorized: Invalid credentials. Check your email and password and try again.' });
+    const thesePasswordsAreTheSame = await compare(password, user.password);
+    if(!thesePasswordsAreTheSame)
+      return response.status(401).send({ message: 'Unauthorized: Invalid credentials. Check your email and password and try again.' });
 
-  user.password = '';
+    user.password = '';
 
-  const token = new Token().tokenJWT(user.id);
+    const token = new Token().tokenJWT(user.id);
 
-  response.status(200).send({ message: 'Success: User authentication successful.', user, token });
-});
+    response.status(200).send({ message: 'Success: User authentication successful.', user, token });
+  }
+);
 
 export default router;
