a31cbd57580c23a91707ab8274933b5cb3b166a0
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-09 23:17:57 -0300

    feat: inserting the product route in the index

--- src/router/index.routes.ts
+++ src/router/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './user.routes';
 import authRouter from './auth.routes';
+import productRouter from './product.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
 routes.use('/auth', authRouter);
+routes.use('/products', productRouter);
 
 export default routes;
