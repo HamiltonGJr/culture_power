047c9fc87647b7a21e645ecd7941730a86c279a2
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-15 20:32:01 -0300

    feat: including methods in the service, where I am using it in the product route

--- src/service/interfaces/IProducts.service.ts
+++ src/service/interfaces/IProducts.service.ts
@@ -1,3 +1,13 @@
 export interface IProductService {
   create: (name: string, value: number, amount: number, description: string, photo: string) => object;
+
+  update: (id: string, name: string, value: number, amount: number, description: string, photo: string) => object;
+
+  userById: (id: string) => object;
+
+  productUpdated: (user: object) => object;
+
+  find: () => object;
+
+  findId: (id: string) => object;
 };
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -10,4 +10,24 @@ export class ProductService implements IProductService {
   async create(name: string, value: number, amount: number, description: string, photo: string) {
     return await this.repository.save(name, value, amount, description, photo);
   };
+
+  async update(id: string, name: string, value: number, amount: number, description: string, photo: string) {
+    return await this.repository.findByIdAndUpdate(id, name, value, amount, description, photo);
+  }
+
+  async userById (id: string) {
+    return await this.repository.findUserById(id);
+  };
+
+  async productUpdated(product: object) {
+    return await this.repository.productUpdated(product);
+  };
+
+  async find() {
+    return await this.repository.findAll();
+  };
+
+  async findId(id: string) {
+    return await this.repository.findByID(id);
+  };
 };
