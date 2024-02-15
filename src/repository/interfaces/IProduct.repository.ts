68997c4eb21eea57650ed1efe1d71f4c85b5b6b0
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-15 20:31:42 -0300

    feat: including methods in the repository, where I am using them in the product route

--- src/repository/interfaces/IProduct.repository.ts
+++ src/repository/interfaces/IProduct.repository.ts
@@ -1,3 +1,13 @@
 export interface IProductRepository {
   save: (name: string, value: number, amount: number, description: string, photo: string) => object;
+
+  findByIdAndUpdate: (id: string, name: string, value: number, amount: number, description: string, photo: string) => object;
+
+  findUserById: (id: string) => object;
+
+  productUpdated: (product: object) => object;
+
+  findAll: () => object;
+
+  findByID: (id: string) => object;
 };
--- src/repository/product.repository.ts
+++ src/repository/product.repository.ts
@@ -5,4 +5,24 @@ export class ProductRepository implements IProductRepository {
   async save(name: string, value: number, amount: number, description: string, photo: string) {
     return await new Product({ name, value, amount, description, photo }).save();
   };
+
+  async findByIdAndUpdate(id: string, name: string, value: number, amount: number, description: string, photo: string) {
+    return await Product.findByIdAndUpdate(id, { name, value, amount, description, photo }).exec();
+  }
+
+  async findUserById(id: string) {
+    return await Product.findById(id).exec();
+  };
+
+  async productUpdated(product: object) {
+    return await new Product(product).save();
+  };
+
+  async findAll() {
+    return await Product.find();
+  };
+
+  async findByID(id: string) {
+    return await Product.findById(id);
+  };
 };
