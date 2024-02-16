cfb09406ca6fb9f6fc1ffc05847685f0eb890a30
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-16 18:27:35 -0300

    feat: from the jewelry route

---
+++ src/schema/amount.schema.ts
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+import { TypedRequest } from '../util/typedRequest';
+
+const jewelsAmountSchema = Yup.object({
+  jewelsAmount: Yup.number()
+});
+
+export namespace CreatePerson {
+  export type BodyType = TypedRequest<typeof jewelsAmountSchema>;
+  export const schema = Yup.object().shape({ params: jewelsAmountSchema });
+};
