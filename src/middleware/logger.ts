d5356b2f9522f8797e6c5b546e04805fa5ca802f
Hamilton Gonçalves <hamilton27089@gmail.com>
2024-02-16 18:24:41 -0300

    refactor: cleaning up the code

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function logger(request: Request, response: Response, next: NextFunction) {
-  console.log('--------------------------------------');
   console.log('Time: ', new Date());
   console.log(`Route: ${request.method}; ${request.path};`);
   console.log('--------------------------------------');
